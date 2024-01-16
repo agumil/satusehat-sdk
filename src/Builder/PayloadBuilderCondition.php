5edb0880a6ddee130793b88ca6267aee11a504f8
Agung Gumilang <agung96gumilang@gmail.com>
2024-01-16 14:16:46 +0700

    add payload builder condition

--- src/Builder/PayloadBuilderCondition.php
+++ src/Builder/PayloadBuilderCondition.php
@@ -1,13 +1,67 @@
 <?php
 namespace agumil\SatuSehatSDK\Builder;
 
+use agumil\SatuSehatSDK\DataType\CodeableConcept;
+use agumil\SatuSehatSDK\DataType\Identifier;
+use agumil\SatuSehatSDK\DataType\Reference;
+
 class PayloadBuilderCondition
 {
     private static $resource_type = 'Condition';
 
+    private array $identifier;
+
+    private array $code;
+
+    private array $subject;
+
+    private array $encounter;
+
+    public function addIdentifier(Identifier $identifier)
+    {
+        $this->identifier[] = $identifier->toArray();
+
+        return $this;
+    }
+
+    public function setCode(CodeableConcept $code)
+    {
+        $this->code = $code->toArray();
+
+        return $this;
+    }
+
+    public function setSubject(Reference $subject)
+    {
+        $this->subject = $subject->toArray();
+
+        return $this;
+    }
+
+    public function setEncounter(Reference $encounter)
+    {
+        $this->encounter = $encounter->toArray();
+    }
+
     public function build(): array
     {
         $data['resourceType'] = self::$resource_type;
+
+        if (!empty($this->identifier)) {
+            $data['identifier'] = $this->identifier;
+        }
+
+        if (!empty($this->code)) {
+            $data['code'] = $this->code;
+        }
+
+        if (!empty($this->subject)) {
+            $data['subject'] = $this->subject;
+        }
+
+        if (!empty($this->encounter)) {
+            $data['encounter'] = $this->encounter;
+        }
 
         return $data;
     }
