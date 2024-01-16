8dc3c15aa18d92cc6337cf401923af10199e790d
Agung Gumilang <agung96gumilang@gmail.com>
2024-01-16 14:17:07 +0700

    add kemkes identifier system standard

---
+++ src/Terminology/KemKes/IdentifierSystem.php
@@ -0,0 +1,45 @@
+<?php
+namespace agumil\SatuSehatSDK\Terminology\KemKes;
+
+class IdentifierSystem
+{
+    public static function organization(string $organizationId): string
+    {
+        return 'http://sys-ids.kemkes.go.id/organization/' . $organizationId;
+    }
+
+    public static function location(string $locationId): string
+    {
+        return 'http://sys-ids.kemkes.go.id/location/' . $locationId;
+    }
+
+    public static function patientNIK(): string
+    {
+        return 'https://fhir.kemkes.go.id/id/nik';
+    }
+
+    public static function patientNIKIbu(): string
+    {
+        return 'https://fhir.kemkes.go.id/id/nik-ibu';
+    }
+
+    public static function patientPaspor(): string
+    {
+        return 'https://fhir.kemkes.go.id/id/paspor';
+    }
+
+    public static function patientIHS(): string
+    {
+        return 'https://fhir.kemkes.go.id/id/ihs-number';
+    }
+
+    public static function encounter(string $organizationId): string
+    {
+        return 'http://sys-ids.kemkes.go.id/encounter/' . $organizationId;
+    }
+
+    public static function condition(string $organizationId): string
+    {
+        return 'http://sys-ids.kemkes.go.id/condition/' . $organizationId;
+    }
+}
