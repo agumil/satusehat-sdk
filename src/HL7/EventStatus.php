c7770ca16c8914494c70efc371040fb81d8983a5
Agung Gumilang <agung96gumilang@gmail.com>
2024-01-24 22:21:59 +0700

    add HL7 terminologies

---
+++ src/HL7/EventStatus.php
@@ -0,0 +1,49 @@
+<?php
+namespace agumil\SatuSehatSDK\HL7;
+
+use agumil\SatuSehatSDK\Interface\HL7Interface;
+
+class EventStatus implements HL7Interface
+{
+    const VERSION = '5.0.0';
+    const SYSTEM = 'http://hl7.org/fhir/event-status';
+
+    const CODE_PREPARATION = 'preparation';
+    const CODE_IN_PROGRESS = 'in-progress';
+    const CODE_NOT_DONE = 'not-done';
+    const CODE_ON_HOLD = 'on-hold';
+    const CODE_STOPPED = 'stopped';
+    const CODE_COMPLETED = 'completed';
+    const CODE_ENTERED_IN_ERROR = 'entered-in-error';
+    const CODE_UNKNOWN = 'unknown';
+
+    public static function getCodes(): array
+    {
+        return [
+            self::CODE_PREPARATION,
+            self::CODE_IN_PROGRESS,
+            self::CODE_NOT_DONE,
+            self::CODE_ON_HOLD,
+            self::CODE_STOPPED,
+            self::CODE_COMPLETED,
+            self::CODE_ENTERED_IN_ERROR,
+            self::CODE_UNKNOWN,
+        ];
+    }
+
+    public static function getDisplayCode(string $code): null | string
+    {
+        $displays = [
+            self::CODE_PREPARATION => 'Preparation',
+            self::CODE_IN_PROGRESS => 'In Progress',
+            self::CODE_NOT_DONE => 'Not Done',
+            self::CODE_ON_HOLD => 'On Hold',
+            self::CODE_STOPPED => 'Stopped',
+            self::CODE_COMPLETED => 'Completed',
+            self::CODE_ENTERED_IN_ERROR => 'Entered in Error',
+            self::CODE_UNKNOWN => 'Unknown',
+        ];
+
+        return @$displays[$code];
+    }
+}
---
+++ src/HL7/SpecimenStatus.php
@@ -0,0 +1,37 @@
+<?php
+namespace agumil\SatuSehatSDK\HL7;
+
+use agumil\SatuSehatSDK\Interface\HL7Interface;
+
+class EventStatus implements HL7Interface
+{
+    const VERSION = '5.0.0';
+    const SYSTEM = 'http://hl7.org/fhir/specimen-status';
+
+    const CODE_AVAILABLE = 'available';
+    const CODE_UNAVAILABLE = 'unavailable';
+    const CODE_UNSATISFACTORY = 'unsatisfactory';
+    const CODE_ENTERED_IN_ERROR = 'entered-in-error';
+
+    public static function getCodes(): array
+    {
+        return [
+            self::CODE_AVAILABLE,
+            self::CODE_UNAVAILABLE,
+            self::CODE_UNSATISFACTORY,
+            self::CODE_ENTERED_IN_ERROR,
+        ];
+    }
+
+    public static function getDisplayCode(string $code): null | string
+    {
+        $displays = [
+            self::CODE_AVAILABLE => 'Available',
+            self::CODE_UNAVAILABLE => 'Unavailable',
+            self::CODE_UNSATISFACTORY => 'Unsatisfactory',
+            self::CODE_ENTERED_IN_ERROR => 'Entered in Error',
+        ];
+
+        return @$displays[$code];
+    }
+}
