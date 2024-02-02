b39a22094a5744efcf9f73632d9dac6877d405c8
Agung Gumilang <agung96gumilang@gmail.com>
2024-02-02 11:16:37 +0700

    improve validator

--- src/Helper/ValidatorHelper.php
+++ src/Helper/ValidatorHelper.php
@@ -1,11 +1,19 @@
 <?php
 namespace agumil\SatuSehatSDK\Helper;
 
+use agumil\SatuSehatSDK\Exception\SSDataTypeException;
+
 class ValidatorHelper
 {
-    public static function validDateTime($var)
+    public static function validDateTime($var, bool $is_return = true)
     {
         $epochStart = strtotime($var);
-        return $epochStart !== false;
+        if ($is_return) {
+            return $epochStart !== false;
+        }
+
+        if ($epochStart === false) {
+            throw new SSDataTypeException('Data type dateTime is unparseable by strtotime. Please provide a valid date, dateTime, or time.');
+        }
     }
 }
