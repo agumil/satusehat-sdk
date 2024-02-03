d5f16ca5e231eb689001bf97b748dd18e51d27cb
Agung Gumilang <agung96gumilang@gmail.com>
2024-02-03 10:53:47 +0700

    add method 'in' validator helper

--- src/Helper/ValidatorHelper.php
+++ src/Helper/ValidatorHelper.php
@@ -16,4 +16,16 @@ class ValidatorHelper
             throw new SSDataTypeException('Data type dateTime is unparseable by strtotime. Please provide a valid date, dateTime, or time.');
         }
     }
+
+    public static function in($needle, array $haystack, bool $is_return = true)
+    {
+        if ($is_return) {
+            return in_array($needle, $haystack);
+        }
+
+        if (!in_array($needle, $haystack)) {
+            $stack_str = implode(',', $haystack);
+            throw new SSDataTypeException("Provided value must be one of {$stack_str}");
+        }
+    }
 }
