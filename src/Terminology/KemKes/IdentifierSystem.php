602912332a1ce357c293371ce6a4294f30da8e1d
Agung Gumilang <agung96gumilang@gmail.com>
2024-02-05 11:06:12 +0700

    add identifier system

--- src/Terminology/KemKes/IdentifierSystem.php
+++ src/Terminology/KemKes/IdentifierSystem.php
@@ -47,4 +47,39 @@ class IdentifierSystem
     {
         return 'http://sys-ids.kemkes.go.id/allergy/' . $organizationId;
     }
+
+    public static function observation(string $organizationId): string
+    {
+        return 'http://sys-ids.kemkes.go.id/observation/' . $organizationId;
+    }
+
+    public static function procedure(string $organizationId): string
+    {
+        return 'http://sys-ids.kemkes.go.id/procedure/' . $organizationId;
+    }
+
+    public static function composition(string $organizationId): string
+    {
+        return 'http://sys-ids.kemkes.go.id/composition/' . $organizationId;
+    }
+
+    public static function medication(string $organizationId): string
+    {
+        return 'http://sys-ids.kemkes.go.id/medication/' . $organizationId;
+    }
+
+    public static function medicationdispense(string $organizationId): string
+    {
+        return 'http://sys-ids.kemkes.go.id/medicationdispense/' . $organizationId;
+    }
+
+    public static function prescription(string $organizationId): string
+    {
+        return 'http://sys-ids.kemkes.go.id/prescription/' . $organizationId;
+    }
+
+    public static function prescriptionitem(string $organizationId): string
+    {
+        return 'http://sys-ids.kemkes.go.id/prescription-item/' . $organizationId;
+    }
 }
