0a6362cb1a706bb8bef3a98ed8944021a881046e
Agung Gumilang <agung96gumilang@gmail.com>
2024-02-16 21:59:21 +0700

    feat: add method random; alpha|numeric|alnum|alnumsym

--- src/Helper/StrHelper.php
+++ src/Helper/StrHelper.php
@@ -22,4 +22,81 @@ class StrHelper
             return json_last_error() === JSON_ERROR_NONE;
         }
     }
+
+    public static function random(int $length = 8, string $type = 'alnum')
+    {
+        if ($length <= 0) {
+            return '';
+        }
+
+        $alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $numeric = '0123456789';
+        $symbol = '~!@#$%&*=_+-';
+
+        if ($length == 1) {
+            $result = $alpha . $numeric . $symbol;
+
+            return $result[rand(0, strlen($result))];
+        }
+
+        $result = '';
+        switch ($type) {
+            case 'alpha':
+                while (strlen($result) < $length) {
+                    $result .= $alpha[rand(0, strlen($alpha) - 1)];
+                }
+                break;
+
+            case 'numeric':
+                while (strlen($result) < $length) {
+                    $result .= $numeric[rand(0, strlen($numeric) - 1)];
+                }
+                break;
+
+            case 'alnumsym':
+                if ($length < 3) {
+                    $alpha_len = rand(0, 1);
+                    $numeric_len = rand(0, 1);
+                    $symbol_len = $length - $alpha_len - $numeric_len;
+                } elseif ($length == 3) {
+                    $alpha_len = 1;
+                    $numeric_len = 1;
+                    $symbol_len = 1;
+                } else {
+                    $alpha_len = rand(1, $length - 2);
+                    $numeric_len = rand(1, ($length - $alpha_len) - 1);
+                    $symbol_len = $length - $alpha_len - $numeric_len;
+                }
+
+                $result = '';
+                for ($i = 0; $i < $alpha_len; $i++) {
+                    $result .= $alpha[rand(0, strlen($alpha) - 1)];
+                }
+
+                for ($i = 0; $i < $numeric_len; $i++) {
+                    $result .= $numeric[rand(0, strlen($numeric) - 1)];
+                }
+
+                for ($i = 0; $i < $symbol_len; $i++) {
+                    $result .= $symbol[rand(0, strlen($symbol) - 1)];
+                }
+                break;
+
+            default:
+                $alpha_len = rand(1, $length - 1);
+                $numeric_len = $length - $alpha_len;
+
+                $result = '';
+                for ($i = 0; $i < $alpha_len; $i++) {
+                    $result .= $alpha[rand(0, strlen($alpha) - 1)];
+                }
+
+                for ($i = 0; $i < $numeric_len; $i++) {
+                    $result .= $numeric[rand(0, strlen($numeric) - 1)];
+                }
+                break;
+        }
+
+        return str_shuffle($result);
+    }
 }
